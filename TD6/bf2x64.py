fc78d4327732047b84aea05c26398d389a9b0889
John Levy <john.levy@thon.polytechnique.fr>
2022-12-05 15:33:06 +0100

    Functioning compiler, no optimization

--- TD6/bf2x64.py
+++ TD6/bf2x64.py
@@ -1,124 +1,108 @@
 #! /usr/bin/env python3
 
-
 import argparse
 import os
 import sys
 
 from brainfuck import BFBackward, BFDecrement, BFExit, BFForward, BFIncrement, BFInput, BFInstruction, BFLoop, BFMemory, BFPrint
 
+#Classes for Intermediate representation
+
 class Instruction:
-    def __init__(self, offset : int, arithmetic, p=False):
+    def __init__(self, offset: int):
+        self.offset = offset
+        
+class Arithmetic(Instruction):
+    def __init__(self, offset : int, arithmetic):
         self.offset = offset
         self.arithmetic = abs(arithmetic)
         if arithmetic < 0:
             self.operator = '-'
-        elif arithmetic >= 0 and not p:
+        elif arithmetic >= 0:
             self.operator = '+'
-        if arithmetic == 0 and p:
-            self.operator = "print"
 
-    def __str__(self):
-        if self.operator == "print":
-            return f"print data[{self.offset}]"
-        else:
-            return f"data[{self.offset}] {self.operator} {self.arithmetic}"
-
-    def __repr__(self) -> str:
-        if self.operator == "print":
-            return f"print data[{self.offset}]"
-        else:
-            return f"data[{self.offset}] {self.operator} {self.arithmetic}"
-
-class Input:
+class Pointer(Instruction):
     def __init__(self, offset: int):
         self.offset = offset
+        self.operator = 'move'
+
+class Input(Instruction):
+    def __init__(self):
+        self.offset = None
         self.operator = "input"
 
-    def __repr__(self):
-        return f"Current offset : {self.offset}, user input"
+class Print(Instruction):
+    def __init__(self):
+        self.offset = None
+        self.operator = "print"
 
 class Loop:
     def __init__(self, offset : int, body : list) -> None:
-        self.offset = offset
+        self.offset = None
         self.operator = "loop"
         self.body = body
+        self.scan_loop = False
+        self.f = None
 
-    def __repr__(self):
-        return f"Current offset : {self.offset}, adding a {self.operator}"
-
+#---------------------------------------------------------------------------
+#Translation to IR 
 
 def translate_instr(program : list, offset=0):
-    #print(f"Program is {program}")
     index = 0
     instr_list = []
     curr_instr = program[0]
     offset_buffer = offset
-    arithmetic_buffer = 0 #init(curr_instr)
+    arithmetic_buffer = 0
 
     while(index != len(program)):
         curr_instr = program[index]
         if index != len(program)-1:
             next_instr = program[index+1]
         else: next_instr = None
+
         if is_incr(curr_instr):
             arithmetic_buffer = arithmetic_buffer + 1 if isinstance(curr_instr, BFIncrement) else arithmetic_buffer - 1
             if is_ptr(next_instr) or is_print(next_instr) or is_loop(next_instr) or index == len(program)-1:
                 emit(instr_list, offset_buffer, arithmetic_buffer)
+                emit_ptr(instr_list, offset_buffer)
                 arithmetic_buffer = 0
-            curr_instr = next_instr
+                offset_buffer = 0
         elif is_ptr(curr_instr):
+            tmp = offset_buffer
             offset_buffer = offset_buffer + 1 if isinstance(curr_instr, BFForward) else offset_buffer -1
-            curr_instr = next_instr
+            if is_loop(next_instr) or index == len(program)-1 or is_print(next_instr):
+                emit_ptr(instr_list, offset_buffer)
+                offset_buffer = 0
         elif is_print(curr_instr):
-            emit(instr_list, offset_buffer, arithmetic_buffer, True)
-            curr_instr = next_instr
+            emit_print(instr_list, offset_buffer)
         elif is_loop(curr_instr):
             body, off = translate_instr(curr_instr.body.block, offset_buffer) # Translate body into list of instructions just as before 
             emit_loop(instr_list, offset_buffer, body)
         elif is_input(curr_instr):
             emit_input(instr_list, offset_buffer)
-
-
-
-
-
-        #curr_instr = next_instr
         index += 1
-        #print(offset_buffer, arithmetic_buffer)
-
-    if is_ptr(curr_instr):
-        emit(instr_list, offset_buffer, arithmetic_buffer)
-
     return instr_list, offset_buffer
 
-def emit(instr_list, offset, arithmetic, p=False):
-    instr_list.append(Instruction(offset, arithmetic, p))
+def emit(instr_list, offset, arithmetic):
+    instr_list.append(Arithmetic(offset, arithmetic))
+
+def emit_ptr(instr_list, offset):
+    instr_list.append(Pointer(offset))
 
 def emit_loop(instr_list, offset, body):
     instr_list.append(Loop(offset, body))
 
 def emit_input(instr_list, offset):
-    instr_list.append(Input(offset))
+    instr_list.append(Input())
 
-def init(instruction):
-    if is_ptr(instruction):
-        if isinstance(instruction, BFForward):
-            return (1, 0)
-        else:
-            return (-1, 0)
-    else:
-        if isinstance(instruction, BFIncrement):
-            return (0, 1)
-        else:
-            return (0, -1)
+def emit_print(instr_list, offset):
+    instr_list.append(Print())
 
 def is_ptr(instruction):
     if isinstance(instruction, BFBackward) or isinstance(instruction, BFForward):
         return True
     else: 
         return False
-
 
 def is_incr(instruction):
     if isinstance(instruction, BFDecrement) or isinstance(instruction, BFIncrement):
@@ -142,9 +126,33 @@ def is_input(instruction):
     else:
         return False
         
+#-----------------------------------------------------------------------------------------------------
+#Optimization 
 
-def to_asm(instructions : list, last_label =0):
-    #print('tets', instructions[0].offset)
+
+def scan_loop_opt(instructions : list):
+    index = 0
+    tmp = []
+    while(index != len(instructions)):
+        curr_instr = instructions[index]
+        if isinstance(curr_instr, Loop) and isinstance(curr_instr.body[0], Pointer):
+            curr_instr.scan_loop = True
+            if curr_instr.body[0].offset == 1:
+                curr_instr.f = True
+            elif curr_instr.body[0].offset == -1:
+                curr_instr.f = False
+        tmp.append(curr_instr)
+        index += 1
+    return tmp
+
+def optimize(prog : list):
+    return scan_loop_opt(prog)
+    
+#-----------------------------------------------------------------------------------------------------
+#Translation to x64
+
+def to_asm(instructions : list, last_label =0, offset = 0):
+    ptr = f"%r15"
     asm = []
     if last_label == 0:
         asm.extend([f"\t.bss", 
@@ -152,76 +160,77 @@ def to_asm(instructions : list, last_label =0):
                     f"\t.zero 30000\n", 
                     f"\t.text", 
                     f"\t.globl main", 
-                    f"main:"
-        ])
-    current_offset = 0
+                    f"main:", 
+                    f"\t pushq %rbp", 
+                    f"\t movq %rsp, %rbp", 
+                    f"\t leaq buffer(%rip), {ptr}"])
+    current_offset = offset
     for instruction in instructions:
-        print(instruction)
+        current_offset = instruction.offset if instruction.offset is not None else current_offset
+        #print(instruction, ":", instruction.offset)
+        #print("curr", current_offset)
+        cell = f"{current_offset}({ptr})" if current_offset != 0 else f"({ptr})"
 
-        print("instrucion : ", instruction.offset)
-        if current_offset + instruction.offset < 0:
-            print("back overflow")
-            current_offset = 30000 + instruction.offset
-            print(current_offset)
-        elif current_offset + instruction.offset > 30000:
-            print("overflow")
-            current_offset = 30000 - (current_offset + instruction.offset)
-            print(current_offset)
-        # else: 
-        #     current_offset = instruction.offset
-
-        cell = f"(buffer+{instruction.offset})(%rip)"
 
         if instruction.operator == "+":
-            asm.extend([f"\t xor %al, %al",
-                        f"\t mov {cell}, %al", 
-                        f"\t add ${instruction.arithmetic}, %al",
-                        f"\t mov %al, {cell}"])
-            pass
+            asm.extend([f"\t addb ${instruction.arithmetic}, {cell}"
+            ])
         elif instruction.operator == "-":
-            asm.extend([f"\t mov {cell}, %al", 
-                        f"\t sub ${instruction.arithmetic}, %al",
-                        f"\t mov %al, {cell}"])
+            asm.extend([f"\t subb ${instruction.arithmetic}, {cell}"])
         elif instruction.operator == "print":
             asm.extend([f"\t xor %dil, %dil",
-                        f"\t mov {cell}, %dil",
-                        f"\t call bf_print"])
-        elif instruction.operator == "loop":
-            last_label +=1
-            l_entry = f".L{last_label}"
-            last_label +=1
-            l_out = f".L{last_label}"
-            asm.extend([f"{l_entry}:", 
-                        f"\t mov {cell}, %al",
-                        f"\t mov $0, %bl", 
-                        f"\t cmp %al, %bl",
-                        f"\t je {l_out}"])
-            body, label_offset = to_asm(instruction.body, last_label)
-            asm.extend(body)
-            asm.extend([f"\t jmp {l_entry}"])
-            asm.extend([f"{l_out}:"])
-            last_label = label_offset
-
+                        f"\t movb ({ptr}), %dil",
+                        f"\t callq putchar"])
         elif instruction.operator == "input":
             asm.extend([f"\t xor %al, %al",
-                        f"\t call bf_read", 
-                        f"\t mov %al, {cell}"])    
+                        f"\t callq getchar", 
+                        f"\t mov %al, ({ptr})"])
 
-            # asm.extend([f"\t xor %dil, %dil",
-            # f"\t mov {cell}, %dil",
-            # f"\t call bf_print"])
+        elif instruction.operator == "move":
+            asm.extend([f"\t addq ${instruction.offset}, {ptr}"])
+
+        elif instruction.operator == "loop":
+
+            if instruction.scan_loop:
+                asm.extend([f"\t leaq buffer(%rip), %r11", 
+                            f"\t movq {ptr}, %r10", 
+                            f"\t movq %r11, %r9", 
+                            f"\t addq $29999, %r9"])
+                if instruction.f:
+                    asm.extend([f"\t movq %r10, %rdi", 
+                                f"\t movb $0, %sil"
+                                f"\t movq %r9, %r8", 
+                                f"\t subq %r10, %r8", 
+                                f"\t movq %r8, %rdx",
+                                f"\t callq memchr"])
+                else:
+                    asm.extend([f"\t movq %r11, %rdi",
+                                f"\t movb $0, %sil",
+                                f"\t movq %r10, %r8", 
+                                f"\t subq %r11, %r8", 
+                                f"\t movq %r8, %rdx",
+                                f"\t callq memrchr"])
+                asm.extend([f"\t movq %rax, {ptr}"])
+            else:
+                last_label +=1
+                l_entry = f".L{last_label}"
+                last_label +=1
+                l_out = f".L{last_label}"
+                asm.extend([f"{l_entry}:", 
+                            f"\t cmpb $0, ({ptr})",
+                            f"\t je {l_out}"])
+                #print("entering recursion", current_offset)
+                body, label_offset = to_asm(instruction.body, last_label, current_offset)
+                #print("exited recursions")
+                asm.extend(body)
+                asm.extend([f"\t jmp {l_entry}"])
+                asm.extend([f"{l_out}:"])
+                last_label = label_offset
+ 
     return asm, last_label
 
-
-
-
-
-
-
-
-
-            
-
+#-----------------------------------------------------------------------------------------------------
+#Driver Code
 
 def _main():
     ap = argparse.ArgumentParser()
@@ -237,7 +246,6 @@ def _main():
         exit(1)
     
     bf_filename = options.fname[0]
-
     if not bf_filename.endswith('.bf'):
         print(f'File {bf_filename} does not have .bf extension')
         sys.exit(1)
@@ -249,13 +257,20 @@ def _main():
     if(options.verbosity):
         print(f"After parsing by brainfuck.py the obtained object is: {program}")
 
+
     intermediate, current_offset = translate_instr(program.block)
 
+    #intermediate = optimize(intermediate)
+
     if(options.verbosity):
-        print(f"After translation to intermediate representation, the list of instructions is: {intermediate}, the data pointer will be changed by {current_offset}")
+        print(f"After translation to intermediate representation & optimization, the list of instructions is: {intermediate}, the data pointer will be changed by {current_offset}")
     
     asm, tmp = to_asm(intermediate)
-    asm.extend([f"\tretq"]) 
+
+    asm.extend([f"\t xorq %rax, %rax", 
+                f"\t movq %rbp, %rsp", 
+                f"\t popq %rbp",
+                f"\t retq"]) 
 
     if(not options.save_asm):
         print(f"ASM GENERATION\n=============")
@@ -272,7 +287,7 @@ def _main():
 
 
     if (options.compile):
-        shell_command = f"gcc -o {assembly_name[:-2]} {assembly_name} bf_runtime.c"
+        shell_command = f"gcc -o {assembly_name[:-2]} {assembly_name} "
         gcc_stat = os.system(shell_command)
         if not (os.WIFEXITED(gcc_stat) and os.WEXITSTATUS(gcc_stat) == 0):
             print(f'ERROR: gcc exited abnormally')
@@ -280,11 +295,6 @@ def _main():
     if (not options.save_asm):
         cleanup = f"rm {assembly_name}"
         os.system(cleanup)
-
-    # try:
-    #     program.execute(BFMemory())
-    # except BFExit:
-    #    pass
 
 # --------------------------------------------------------------------
 if __name__ == '__main__':
